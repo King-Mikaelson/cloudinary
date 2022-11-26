dfeded53c709803b03a144dec0ec9af78c47d67b
King-Mikaelson <anazodomichael27@gmail.com>
2022-11-26 12:15:06 +0100

    Added New Route

--- Api/routes/detailsUpload.js
+++ Api/routes/detailsUpload.js
@@ -5,5 +5,10 @@ const Uploads = require('../Controller/detailsUpload')
 router.get('/get-images', Uploads.getImage)
 router.post('/uploadFile', Uploads.upload, Uploads.uploadImage)
 router.post('/register-user', Uploads.registerUser)
+router.get('/ping', (req, res) => {
+    res.json({
+        "message" : "GDG Enugu Cloudinary server working"
+    })
+})
 
 module.exports = router
